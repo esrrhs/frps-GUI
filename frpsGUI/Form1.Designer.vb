90911ea99f6c6ff0ece49fd5060ab2c196a449e0
esrrhs <esrrhs@163.com>
2019-06-07 20:05:53 +0800

    add

---
+++ frpsGUI/Form1.Designer.vb
@@ -0,0 +1,83 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Main))
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.SToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Switch = New System.Windows.Forms.Button()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "frp GUI 1.0.20"
+        Me.NotifyIcon1.Visible = True
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.ImageScalingSize = New System.Drawing.Size(18, 18)
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SToolStripMenuItem1})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(97, 26)
+        '
+        'SToolStripMenuItem1
+        '
+        Me.SToolStripMenuItem1.Name = "SToolStripMenuItem1"
+        Me.SToolStripMenuItem1.Size = New System.Drawing.Size(96, 22)
+        Me.SToolStripMenuItem1.Text = "Exit"
+        '
+        'Switch
+        '
+        Me.Switch.Location = New System.Drawing.Point(12, 6)
+        Me.Switch.Name = "Switch"
+        Me.Switch.Size = New System.Drawing.Size(75, 23)
+        Me.Switch.TabIndex = 8
+        Me.Switch.Text = "Start"
+        Me.Switch.UseVisualStyleBackColor = True
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(97, 35)
+        Me.Controls.Add(Me.Switch)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MaximizeBox = False
+        Me.Name = "Main"
+        Me.Opacity = 0R
+        Me.ShowInTaskbar = False
+        Me.Text = "frps GUI"
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents SToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents Switch As Button
+End Class
---
+++ frpsGUI/Form1.vb
@@ -0,0 +1,137 @@
+﻿Imports System
+Imports System.IO
+Imports System.Text
+Imports System.Reflection
+Imports System.Xml
+
+Public Class Main
+    Public path As String = Application.StartupPath()
+
+    Sub terminate()
+        If System.Diagnostics.Process.GetProcessesByName("frps").Length > 0 Then
+            Try
+                Dim i As Integer
+                Dim proc As Process()
+                proc = Process.GetProcessesByName("frps")
+                For i = 0 To proc.Length - 1
+                    proc(i).Kill()
+                Next
+                Switch.Text = "Start"
+            Catch ex As Exception
+
+            End Try
+        End If
+        Application.Exit()
+    End Sub
+
+    Private WithEvents MyProcess As Process
+
+    Private Delegate Sub AppendOutputTextDelegate(ByVal text As String)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Loading code
+        If My.Computer.FileSystem.FileExists(path & "/frps.exe") Then
+        Else
+            MsgBox("need frps.exe", MsgBoxStyle.OkOnly, "frp GUI")
+            Application.Exit()
+            Return
+        End If
+        If My.Computer.FileSystem.FileExists(path & "/frps.ini") Then
+        Else
+            MsgBox("need frps.ini", MsgBoxStyle.OkOnly, "frp GUI")
+            Application.Exit()
+            Return
+        End If
+
+        My.Computer.Registry.CurrentUser.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run", True).SetValue(Application.ProductName, Application.ExecutablePath & " -s")
+
+        If System.Diagnostics.Process.GetProcessesByName("frps").Length > 0 Then
+            Try
+                Dim i As Integer
+                Dim proc As Process()
+                proc = Process.GetProcessesByName("frps")
+                For i = 0 To proc.Length - 1
+                    proc(i).Kill()
+                Next
+                Switch.Text = "Start"
+            Catch ex As Exception
+
+            End Try
+        End If
+
+        Go()
+
+    End Sub
+
+    Protected Overrides Sub OnClosing(ByVal e As System.ComponentModel.CancelEventArgs)
+        Me.Hide()
+        NotifyIcon1.BalloonTipTitle = "Notice"
+        NotifyIcon1.BalloonTipText = "frps GUI is running in the background"
+        NotifyIcon1.ShowBalloonTip(2)
+        e.Cancel = True
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub ExportConfigToolStripMenuItem_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub NotifyIcon1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon1.MouseDoubleClick
+        Me.Show()
+        Me.Opacity = 100%
+        Me.Activate()
+    End Sub
+
+    Private Sub Switch_Click(sender As Object, e As EventArgs) Handles Switch.Click
+        Go()
+    End Sub
+
+    Private Sub Go()
+        If System.Diagnostics.Process.GetProcessesByName("frps").Length > 0 Then
+            Try
+                Dim i As Integer
+                Dim proc As Process()
+                proc = Process.GetProcessesByName("frps")
+                For i = 0 To proc.Length - 1
+                    proc(i).Kill()
+                Next
+                Switch.Text = "Start"
+            Catch ex As Exception
+
+            End Try
+        Else
+            MyProcess = New Process
+            With MyProcess.StartInfo
+                .FileName = "frps.exe"
+                .Arguments = "-c """ & path & "\frps.ini"""
+                .UseShellExecute = False
+                .CreateNoWindow = True
+                .RedirectStandardInput = True
+                .RedirectStandardOutput = True
+                .RedirectStandardError = True
+            End With
+            Control.CheckForIllegalCrossThreadCalls = False
+            MyProcess.Start()
+            Switch.Text = "Stop"
+        End If
+    End Sub
+
+    Private Sub OpenFrpciniToolStripMenuItem_Click(sender As Object, e As EventArgs)
+        Try
+            System.Diagnostics.Process.Start(path & "/frpc.ini")
+        Catch ex As Exception
+            MsgBox("Unable to open frpc.ini. Maybe it was deleted?")
+        End Try
+    End Sub
+
+    Private Sub SToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles SToolStripMenuItem1.Click
+        terminate()
+    End Sub
+
+    Private Sub ContextMenuStrip1_Opening(sender As Object, e As ComponentModel.CancelEventArgs) Handles ContextMenuStrip1.Opening
+
+    End Sub
+End Class
